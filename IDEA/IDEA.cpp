f2058d2aaf84efd2d4e20188299143203e870f2c
kaparow <152067276+kaparow@users.noreply.github.com>
2023-12-11 00:09:40 +0300

    Revert "Upd."
    This reverts commit b8036f927ece2c1ddadf3d34b96406f56aa6341c.

--- IDEA/IDEA.cpp
+++ IDEA/IDEA.cpp
@@ -4,7 +4,29 @@ using namespace std;
 #include "IDEA.h"
 #include <iomanip>
 
+class IDEA
+{
+public:
+	void setKey(byte in[]);
+	void setPlainText(byte in[]);
+	word16 invMul(word16 x);
+	word16 mul(word16 x, word16 y);
+	void encryption(word16 in[], word16 out[], word16* Ek);
+	void enc();
+	void IDEATest();
+	void encrypt(byte key[], byte plainText[]);
 
+private:
+	void getEncRoundKey(word16* encRoundKey);
+	void getDecRoundKey(word16* EK, word16 DK[]);
+	byte key[16];
+	word16 cipherText[4];
+	word16 plainText[4];
+	word16 deCipherText[4];
+	word16 encRoundKey[52];
+	word16 decRoundKey[52];
+	void checkRoundKey();
+};
 
 void IDEA::setPlainText(byte in[]) {
 	// Устанавливает открытый текст в формате byte. 
