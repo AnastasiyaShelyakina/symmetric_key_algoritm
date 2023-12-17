472fe6be06f99b0a394381cefed3a12ff8398dc3
kaparow <152067276+kaparow@users.noreply.github.com>
2023-12-17 20:04:05 +0300

    Full reboot

--- IDEA/IDEA.cpp
+++ IDEA/IDEA.cpp
@@ -1,231 +1,40 @@
-﻿#include <iostream>
-#include <fstream>
-using namespace std;
+﻿// Proiect_SC.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
 #include "IDEA.h"
-#include <windows.h>
-#include <iomanip>
 #include <string>
-#include <vector>
-#include <cstdint>
-#include <sstream>
+
+#define TEST_STRING "aLorem"
 
 
+using namespace std;
 
-void IDEA::setPlainText(byte in[]) {
-	// 8 BYTE type data becomes 4 Word16 data
-	int i;
-	for (i = 0; i < 8; i += 2) {
-		plainText[i / 2] = (in[i] << 8) + in[i + 1];
-	}
+string IDEA_Encrypt(string input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideaencr = ideatest.encrypt();
+    return ideaencr;
+}
+string IDEA_Decrypt(string input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideadecr = ideatest.decrypt(IDEA_Encrypt(input));
+    return ideadecr;
 }
 
-void IDEA::setKey(byte in[]) {
-	int i;
-	for (i = 0; i < 16; i++) {
-		key[i] = in[i];
-	}
-	getEncRoundKey(encRoundKey);
-	getDecRoundKey(encRoundKey, decRoundKey);
-}
-
-void IDEA::getEncRoundKey(word16* encRoundKey) {
-	int i, j;
-	for (i = 0, j = 0; j < 8; j++) {
-		encRoundKey[j] = (key[i] << 8) + key[i + 1];
-		i += 2;
-	}
-	for (i = 0; j < 52; j++) {
-		i++;
-		encRoundKey[i + 7] = encRoundKey[i & 7] << 9 | encRoundKey[(i + 1) & 7] >> 7;
-		encRoundKey += (i & 8);
-		i &= 7;
-	}
-}
-
-void IDEA::getDecRoundKey(word16* EK, word16 DK[]) {
-	int i;
-	word16 temp[52];
-	word16 t1, t2, t3;
-	word16* p = temp + 52;
-	t1 = invMul(*EK++);
-	t2 = -*EK++;
-	t3 = -*EK++;
-	*--p = invMul(*EK++);
-	*--p = t3;
-	*--p = t2;
-	*--p = t1;
-	for (i = 0; i < 7; i++) {
-		t1 = *EK++;
-		*--p = *EK++;
-		*--p = t1;
-		t1 = invMul(*EK++);
-		t2 = -*EK++;
-		t3 = -*EK++;
-		*--p = invMul(*EK++);
-		*--p = t2;
-		*--p = t3;
-		*--p = t1;
-	}
-	t1 = *EK++;
-	*--p = *EK++;
-	*--p = t1;
-	t1 = invMul(*EK++);
-	t2 = -*EK++;
-	t3 = -*EK++;
-	*--p = invMul(*EK++);
-	*--p = t3;
-	*--p = t2;
-	*--p = t1;
-	for (i = 0, p = temp; i < 52; i++) {
-		*EK++ = *p;
-		*p++ = 0;
-	}
-}
-
-word16 IDEA::invMul(word16 x) {
-	word16 t0, t1;
-	word16 q, y;
-	if (x <= 1) {
-		return x; // 0 and 1 inverse itself
-	}
-	t1 = word16(0x10001L / x);
-	y = word16(0x10001L % x);
-	if (y == 1) {
-		return (1 - t1) & 0xFFFF;
-	}
-	t0 = 1;
-	do {
-		q = x / y;
-		x = x % y;
-		t0 += q * t1;
-		if (x == 1) {
-			return t0;
-		}
-		q = y / x;
-		y = y % x;
-		t1 += q * t0;
-	} while (y != 1);
-	return (1 - t1) & 0xFFFF;
-}
-
-void IDEA::encryption(word16 in[], word16 out[], word16* EK) {
-	word16 x1, x2, x3, x4, t1, t2;
-	x1 = in[0];
-	x2 = in[1];
-	x3 = in[2];
-	x4 = in[3];
-	int r = 8;
-	do {
-		x1 = mul(x1, *EK++);
-		x2 += *EK++;
-		x3 += *EK++;
-		x4 = mul(x4, *EK++);
-		t2 = x1 ^ x3;
-		t1 = x2 ^ x4;
-		t2 = mul(t2, *EK++);
-		t1 = t1 + t2;
-		t1 = mul(t1, *EK++);
-		t2 = t1 + t2;
-		x1 ^= t1;
-		x4 ^= t2;
-		t2 ^= x2;
-		x2 = x3 ^ t1;
-		x3 = t2;
-	} while (--r);
-	x1 = mul(x1, *EK++);
-	*out++ = x1;
-	*out++ = x3 + *EK++;
-	*out++ = x2 + *EK++;
-	x4 = mul(x4, *EK++);
-	*out = x4;
-}
-
-word16 IDEA::mul(word16 x, word16 y) {
-	word32 p;
-	p = (word32)x * y;
-	if (p) {
-		y = p & 0xFFFF;
-		x = p >> 16;
-		return (y - x) + (y < x);
-	}
-	else if (x) {
-		return 1 - y;
-	}
-	else {
-		return 1 - x;
-	}
-}
-
-void IDEA::enc() {
-	encryption(plainText, cipherText, encRoundKey);
-	encryption(cipherText, deCipherText, decRoundKey);
-}
-
-std::string IDEA::IDEATest(std::string text) {
-	ostringstream result;
-	for (const auto& word : cipherText) {
-		result << hex << setw(2) << setfill('0') << static_cast<int>((word >> 8) & 0xFF);
-		result << hex << setw(2) << setfill('0') << static_cast<int>(word & 0xFF);
-	}
-
-	// Вернуть зашифрованный текст как строку
-	std::string encryptedText = result.str();
-	return encryptedText;
-}
-std::string IDEA_Encrypted(const std::string& text) {
-	IDEA idea;
-	byte key[16] = { 0x10, 0x1A, 0x0C, 0x0B, 0x01, 0x11, 0x09, 0x07, 0x32, 0xA1,
-		0xB3, 0x06, 0x23, 0x12, 0xD3, 0xF1 };
-	idea.setKey(key);
-
-	std::string mutableText = text; // Копируем строку, чтобы убрать квалификатор const
-	if (mutableText.length() < 8) {
-		mutableText.resize(8, ' ');
-	}
-
-	idea.setPlainText(reinterpret_cast<byte*>(const_cast<char*>(mutableText.data())));
-	idea.enc();
-	return idea.IDEATest(text);
-}
-
-std::string IDEA::IDEADecTest(std::string text) {
-	ostringstream result;
-	for (const auto& word : deCipherText) {
-		result << hex << setw(2) << setfill('0') << static_cast<int>((word >> 8) & 0xFF);
-		result << hex << setw(2) << setfill('0') << static_cast<int>(word & 0xFF);
-	}
-
-	// Вернуть расшифрованный текст как строку
-	std::string decryptedText = result.str();
-	return decryptedText;
-}
-
-std::string IDEA_Decrypted(const std::string& text) {
-	IDEA idea;
-	byte key[16] = { 0x10, 0x1A, 0x0C, 0x0B, 0x01, 0x11, 0x09, 0x07, 0x32, 0xA1,
-		0xB3, 0x06, 0x23, 0x12, 0xD3, 0xF1 };
-	idea.setKey(key);
-
-	std::string mutableText = text; // Копируем строку, чтобы убрать квалификатор const
-	if (mutableText.length() < 8) {
-		mutableText.resize(8, ' ');
-	}
-
-	idea.setPlainText(reinterpret_cast<byte*>(const_cast<char*>(mutableText.data())));
-	idea.enc(); // Используем ту же функцию enc, так как она выполняет и шифрование, и дешифрование
-	return "Hello"; //return IDEADecTest(text)
-}
-
-int main(int argc, char const* argv[])
+int main()
 {
-	SetConsoleOutputCP(1251);
-	// Encrypt the message
-	std::string encryptedMessage = IDEA_Encrypted("Hello");
-	std::cout << "Encrypted: " << encryptedMessage << std::endl;
-
-	// Decrypt the message
-	std::string decryptedMessage = IDEA_Decrypted(encryptedMessage);
-	std::cout << "Decrypted: " << decryptedMessage << std::endl;
-
-	return 0;
+    //  IDEA ---------------------------------------
+    cout << "IDEA ALGORITHM:" << endl;
+    string input = "Kakdela";
+    string ideaencr = IDEA_Encrypt(input);
+    string ideadecr = IDEA_Decrypt(input);
+    cout << "Encrypted: " << ideaencr << endl;
+    cout << "Decrypted: " << ideadecr << endl;
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
--- IDEA/IDEA.h
+++ IDEA/IDEA.h
@@ -1,28 +1,465 @@
-//define the type of data
-typedef unsigned char byte;
-typedef unsigned short word16;//16bit
-typedef unsigned long word32;//32bit
+#pragma once
+#pragma once
+#include <string>
+#include <vector>
 
+using namespace std;
+
+#define ROL32(a, n) (((a) << (n)) | ((a) >> (32 - (n))))
+
+constexpr auto MAX_LIMIT = 65536;
+typedef uint16_t PART;
+typedef unsigned char BIT;
+typedef unsigned char POSITION;
 class IDEA
 {
-public:
-	void setKey(byte in[]);
-	void setPlainText(byte in[]);
-	word16 invMul(word16 x);
-	word16 mul(word16 x, word16 y);
-	void encryption(word16 in[], word16 out[], word16* Ek);
-	void enc();
-	string IDEATest(std::string text);
-	string IDEADecTest(std::string text);
-
 private:
-	void getEncRoundKey(word16* encRoundKey);
-	void getDecRoundKey(word16* EK, word16 DK[]);
-	byte key[16];
-	word16 cipherText[4];
-	word16 plainText[4];
-	word16 deCipherText[4];
-	word16 encRoundKey[52];
-	word16 decRoundKey[52];
-	void checkRoundKey();
+	string message = "";
+	string key = "";
+	vector<PART> subkeys;
+	static const int PARTSIZE = 8;
+
+
+	static BIT getBit(PART var, POSITION pos) {
+		var = var >> (pos - 1);
+		if (var % 2 == 0)
+			return 0;
+		return 1;
+	}
+
+	static void setBit(PART& var, POSITION pos, BIT val) {
+		PART temp = val;
+		temp = temp << (pos - 1);
+		var = var | temp;
+	}
+
+	static BIT getBit(unsigned char var, POSITION pos) {
+		var = var >> (pos - 1);
+		if (var % 2 == 0)
+			return 0;
+		return 1;
+	}
+
+	static void setBit(unsigned char& var, POSITION pos, BIT val) {
+		unsigned char temp = val;
+		temp = temp << (pos - 1);
+		var = var | temp;
+	}
+
+	static void TwoCharsToPART(PART& P, unsigned char ch[]) {
+		int charCounter = 1;
+		int counter = 1;
+		for (int i = 1; i < 17; i++) {
+			unsigned char character = ch[charCounter];
+			setBit(P, i, getBit(character, counter));
+			counter++;
+			if (counter == 9) {
+				counter = 1;
+				charCounter--;
+			}
+		}
+
+	}
+
+	static string PARTtoChars(PART p) {
+		string result = "";
+
+		unsigned char a = 0;
+		unsigned char b = 0;
+
+		for (int i = 1; i < 9; i++) {
+			setBit(a, i, getBit(p, i));
+			setBit(b, i, getBit(p, i + 8));
+		}
+		if (b != 0)
+			result += (char)b;
+		if (a != 0)
+			result += (char)a;
+		return result;
+	}
+
+	static uint16_t multiplyModulus(uint16_t a, uint16_t b) {
+		uint32_t temp;
+		temp = a * b;
+
+		if (temp != 0) {
+			temp = ((ROL32(temp, 16) - temp) >> 16) + 1;
+		}
+		else {
+			temp = 1 - a - b;
+		}
+		return temp & 0xFFFF;
+	}
+
+	static uint16_t inverseMultiply(uint16_t a) {
+		uint32_t b;
+		uint32_t q;
+		uint32_t r;
+		int32_t t;
+		int32_t u;
+		int32_t v;
+		b = 0x10001;
+		u = 0;
+		v = 1;
+
+		while (a > 0) {
+			q = b / a;
+			r = b % a;
+
+			b = a;
+			a = r;
+
+			t = v;
+			v = u - q * v;
+			u = t;
+		}
+
+		if (u < 0) {
+			u += 0x10001;
+		}
+		return u;
+	}
+
+
+
+	static PART additionModulus(PART a, PART b) {
+		return ((uint32_t)a + (uint32_t)b) % MAX_LIMIT;
+	}
+
+	static string encrypt_16in4(unsigned char buffer[], vector<PART> subkeys) {
+		string result = "";
+
+		PART P1 = 0, P2 = 0, P3 = 0, P4 = 0;
+
+		unsigned char TwoCH[2] = { 0 };
+
+		int p_counter = 1;
+		int counter = 0;
+		for (int i = 0; i < sizeof(buffer); i++) {
+			TwoCH[counter] = buffer[i];
+			counter++;
+			if (counter == 2) {
+				if (p_counter == 1)
+					TwoCharsToPART(P1, TwoCH);
+				else if (p_counter == 2)
+					TwoCharsToPART(P2, TwoCH);
+				else if (p_counter == 3)
+					TwoCharsToPART(P3, TwoCH);
+				else if (p_counter == 4)
+					TwoCharsToPART(P4, TwoCH);
+				p_counter++;
+				counter = 0;
+			}
+		}
+		PART K1 = 0;
+		PART K2 = 0;
+		PART K3 = 0;
+		PART K4 = 0;
+		PART K5 = 0;
+		PART K6 = 0;
+		PART temp_1 = 0;
+		PART temp_2 = 0;
+		for (int key_counter = 0; key_counter < subkeys.size(); key_counter += 6) {
+			if (key_counter < 48) {
+				K1 = subkeys[key_counter + 0];
+				K2 = subkeys[key_counter + 1];
+				K3 = subkeys[key_counter + 2];
+				K4 = subkeys[key_counter + 3];
+				K5 = subkeys[key_counter + 4];
+				K6 = subkeys[key_counter + 5];
+
+				P1 = multiplyModulus(P1, K1);				// stepbro 1
+				P2 = additionModulus(P2, K2);;				// stepbro 2
+				P3 = additionModulus(P3, K3);;				// stepbro 3
+				P4 = multiplyModulus(P4, K4);;				// stepbro 4
+				temp_1 = P1 ^ P3;							// stepbro 5
+				temp_2 = P2 ^ P4;							// stepbro 6
+
+				temp_1 = multiplyModulus(temp_1, K5);		// stepbro 7
+				temp_2 = additionModulus(temp_1, temp_2);	// stepbro 8 
+
+				temp_2 = multiplyModulus(temp_2, K6);		// stepbro 9
+				temp_1 = additionModulus(temp_1, temp_2);	// stepbro 10
+
+				P1 = P1 ^ temp_2;							// stepbro 11
+				P3 = P3 ^ temp_2;							// stepbro 12
+				P2 = P2 ^ temp_1;							// stepbro 13
+				P4 = P4 ^ temp_1;							// stepbro 14
+			}
+		}
+		P1 = multiplyModulus(P1, subkeys[48]);
+		P2 = additionModulus(P2, subkeys[49]);
+		P3 = additionModulus(P3, subkeys[50]);
+		P4 = multiplyModulus(P4, subkeys[51]);
+
+		result += PARTtoChars(P1);
+		result += PARTtoChars(P2);
+		result += PARTtoChars(P3);
+		result += PARTtoChars(P4);
+
+		return result;
+	}
+
+	static vector<PART> subkeyGeneration(string key) {
+		if (!key.compare("") || key.length() != 16) {
+			throw "The key must be exactly 16 characters long!";
+		}
+		vector<PART> result;
+		PART parts[8] = { 0 };
+
+		unsigned char temp[2] = { 0 };
+		int counter = 0;
+		int partCounter = 0;
+		for (unsigned char i : key) {
+			temp[counter] = i;
+			counter++;
+			if (counter > 1) {
+				TwoCharsToPART(parts[partCounter], temp);
+				partCounter++;
+				counter = 0;
+			}
+		}
+		for (PART i : parts) {
+			result.push_back(i);
+		}
+
+
+		return result;
+	}
+
+	/*static void printInBit(PART item) {
+		for ( int i = 16; i > 0; i-- ) {
+			if ( i % 8 == 0 && i!=16 ) {
+				cout << " ";
+			}
+			cout << to_string(getBit(item, i));
+		}
+		cout << " ";
+	}*/
+
+
+	static void shift25left(vector<PART>& subkey) {
+		int subkey_length = subkey.size();
+		BIT last_bit = 0;
+		BIT first_bit = 0;
+		for (int i = 0; i < 25; i++) {
+			for (int j = subkey_length - 1; j >= 0; j--) {
+				last_bit = getBit(subkey[j], 16);
+				subkey[j] = subkey[j] << 1;
+				if (first_bit == 1) {
+					setBit(subkey[j], 1, first_bit);
+				}
+				if (j == 0 && last_bit == 1) {
+					setBit(subkey[subkey_length - 1], 1, last_bit);
+				}
+				first_bit = last_bit;
+			}
+		}
+	}
+	static string decrypt8chars(unsigned char buffer[], vector<PART> subkeys) {
+		string result = "";
+
+		PART P1 = 0, P2 = 0, P3 = 0, P4 = 0;
+
+		unsigned char TwoCH[2] = { 0 };
+
+		int p_counter = 1;
+		int counter = 0;
+		for (int i = 0; i < sizeof(buffer); i++) {
+			TwoCH[counter] = buffer[i];
+			counter++;
+			if (counter == 2) {
+				if (p_counter == 1)
+					TwoCharsToPART(P1, TwoCH);
+				else if (p_counter == 2)
+					TwoCharsToPART(P2, TwoCH);
+				else if (p_counter == 3)
+					TwoCharsToPART(P3, TwoCH);
+				else if (p_counter == 4)
+					TwoCharsToPART(P4, TwoCH);
+				p_counter++;
+				counter = 0;
+			}
+		}
+
+		PART K1 = 0;
+		PART K2 = 0;
+		PART K3 = 0;
+		PART K4 = 0;
+		PART K5 = 0;
+		PART K6 = 0;
+		PART temp_1 = 0;
+		PART temp_2 = 0;
+
+
+		for (int key_counter = 51; key_counter >= 0; key_counter -= 6) {
+			if (key_counter > 3) {
+				K1 = inverseMultiply(subkeys[key_counter - 3]);
+				K2 = -subkeys[key_counter - 2];
+				K3 = -subkeys[key_counter - 1];
+				K4 = inverseMultiply(subkeys[key_counter - 0]);
+				K5 = subkeys[key_counter - 5];
+				K6 = subkeys[key_counter - 4];
+
+				P1 = multiplyModulus(P1, K1);					// stepbro 1
+				P2 = additionModulus(P2, K2);					// stepbro 2
+				P3 = additionModulus(P3, K3);					// stepbro 3
+				P4 = multiplyModulus(P4, K4);					// stepbro 4
+				temp_1 = P1 ^ P3;								// stepbro 5
+				temp_2 = P2 ^ P4;								// stepbro 6
+
+				temp_1 = multiplyModulus(temp_1, K5);			// stepbro 7
+				temp_2 += temp_1;		// stepbro 8 
+
+				temp_2 = multiplyModulus(temp_2, K6);			// stepbro 9
+				temp_1 += temp_2;		// stepbro 10
+
+				P1 = P1 ^ temp_2;								// stepbro 11
+				P3 = P3 ^ temp_2;								// stepbro 12
+				P2 = P2 ^ temp_1;								// stepbro 13
+				P4 = P4 ^ temp_1;								// stepbro 14
+			}
+		}
+
+		P1 = multiplyModulus(P1, inverseMultiply(subkeys[0]));
+		P2 = additionModulus(P2, -subkeys[1]);
+		P3 = additionModulus(P3, -subkeys[2]);
+		P4 = multiplyModulus(P4, inverseMultiply(subkeys[3]));
+
+		result += PARTtoChars(P1);
+		result += PARTtoChars(P2);
+		result += PARTtoChars(P3);
+		result += PARTtoChars(P4);
+
+		return result;
+	}
+
+
+public:
+	IDEA(string message, string key) {
+		setMessage(message);
+		setKey(key);
+	}
+
+	IDEA() {}
+
+	~IDEA() {}
+
+	string getMessage() {
+		return this->message;
+	}
+	string getKey() {
+		return this->key;
+	}
+
+	void setMessage(string message) {
+		int msgLength = message.length();
+		if (msgLength < 16) {
+			for (int i = msgLength; i < 16; i++) {
+				message = message + ' ';
+			}
+		}
+		this->message = message;
+	}
+
+	void setKey(string key) {
+		if (key.length() != 16) {
+			throw "The key must have exactly 16 characters";
+		}
+		this->key = key;
+		this->subkeys.clear();
+		vector<PART> subkey;
+		for (int i = 0; i < 7; i++) {
+			if (i == 0) {
+				subkey = subkeyGeneration(this->key);
+				for (PART k : subkey) {
+					this->subkeys.push_back(k);
+				}
+				continue;
+			}
+			shift25left(subkey);
+			for (PART k : subkey) {
+				this->subkeys.push_back(k);
+			}
+		}
+
+	}
+
+	string encrypt() {
+		string result = "";
+
+		unsigned char buffer[PARTSIZE] = { 0 };
+		int bufcounter = 0;
+		for (char i : this->message) {
+			buffer[bufcounter] = i;
+			bufcounter++;
+			if (bufcounter == PARTSIZE) {
+				result += encrypt_16in4(buffer, this->subkeys);
+				bufcounter = 0;
+				continue;
+			}
+		}
+
+		if (bufcounter != 0) {
+			for (int i = bufcounter; i < PARTSIZE; i++) {
+				buffer[i] = ' ';
+			}
+			result += encrypt_16in4(buffer, this->subkeys);
+		}
+
+		return result;
+	}
+
+	string decrypt(string encrypted_message, vector<PART> subkeys) {
+		string result = "";
+		unsigned char buffer[PARTSIZE] = { 0 };
+		int bufcounter = 0;
+		for (char i : encrypted_message) {
+			buffer[bufcounter] = i;
+			bufcounter++;
+			if (bufcounter == PARTSIZE) {
+				result += decrypt8chars(buffer, subkeys);
+				bufcounter = 0;
+				continue;
+			}
+		}
+
+		if (bufcounter != 0) {
+			for (int i = bufcounter; i < PARTSIZE; i++) {
+				buffer[i] = ' ';
+			}
+			result += decrypt8chars(buffer, subkeys);
+		}
+
+		return result;
+	}
+	string decrypt(string encrypted_message) {
+		string result = "";
+
+		unsigned char buffer[PARTSIZE] = { 0 };
+		int bufcounter = 0;
+		for (char i : encrypted_message) {
+			buffer[bufcounter] = i;
+			bufcounter++;
+			if (bufcounter == PARTSIZE) {
+				result += decrypt8chars(buffer, this->subkeys);
+				bufcounter = 0;
+				continue;
+			}
+		}
+
+		if (bufcounter != 0) {
+			for (int i = bufcounter; i < PARTSIZE; i++) {
+				buffer[i] = ' ';
+			}
+			result += decrypt8chars(buffer, this->subkeys);
+		}
+
+		return result;
+	}
+
+
+
+
 };
+
