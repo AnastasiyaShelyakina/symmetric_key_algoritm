20d7dbeb91b44e0a58339d418e8779881fc84b36
kaparow <152067276+kaparow@users.noreply.github.com>
2023-12-17 20:07:51 +0300

    upd.

---
+++ SymmetricKeyAlgoritm/IDEA.cpp
@@ -0,0 +1,40 @@
+﻿// Proiect_SC.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "IDEA.h"
+#include <string>
+
+#define TEST_STRING "aLorem"
+
+
+using namespace std;
+
+string IDEA_Encrypt(string input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideaencr = ideatest.encrypt();
+    return ideaencr;
+}
+string IDEA_Decrypt(string input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideadecr = ideatest.decrypt(IDEA_Encrypt(input));
+    return ideadecr;
+}
+
+int main()
+{
+    //  IDEA ---------------------------------------
+    cout << "IDEA ALGORITHM:" << endl;
+    string input = "Kakdela";
+    string ideaencr = IDEA_Encrypt(input);
+    string ideadecr = IDEA_Decrypt(input);
+    cout << "Encrypted: " << ideaencr << endl;
+    cout << "Decrypted: " << ideadecr << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
