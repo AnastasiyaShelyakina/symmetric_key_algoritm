a56768c8009be3b5adc2c80155fdc03836606f4e
AnastasiyaShelyakina <shelyakina.as@gmail.com>
2023-12-20 21:40:30 +0300

    coedinili 3 vetki

--- SymmetricKeyAlgoritm/SymmetricKeyAlgoritm.cpp
+++ SymmetricKeyAlgoritm/SymmetricKeyAlgoritm.cpp
@@ -1,37 +1,12 @@
-﻿#include <fstream>
-#include <string>
-#include "Algorithm.h"
-#include "cast128.h"
-#include <iostream>
-#include <iomanip>
-
-
-//void Run(Algorithm a, Algorithm b, std::string author) {
-//
-//    std::string input = "Hello, World";
-//    std::cout << "Original: " << input << std::endl;
-//    std::string encryptedString = a(input);
-//    std::cout << "Encrypted: " << encryptedString << std::endl;
-//    std::string decryptedString = b(encryptedString);
-//    std::cout << "Decrypted: " << decryptedString << std::endl;
-//}
-//
-
-int main() {
-    std::string input = "Hello, World";
-    std::cout << "Original: " << input << std::endl;
-    Run(input);
-    return 0;
-}
-
-﻿#include <iostream>
+﻿#include "AES.h"
+#include "blowfish.h"
+#include "DES.h"
 #include <fstream>
 #include <string>
 #include "Algorithm.h"
-#include "AES.h"
-#include "blowfish.h"
-#include "DES.h"
-/// Если у вас есть заголовочные файлы добавляйте их сюда
+#include "cast128.h"
+#include <iomanip>
+#include <iostream>
 
 
 void Run(Algorithm a, Algorithm b, std::string author) {
@@ -45,10 +20,13 @@ void Run(Algorithm a, Algorithm b, std::string author) {
 }
 int main() {
     std::string input = "Hello, World!";
-    DES des;
+    /*DES des;
     std::string a = des.DES_encryptString(input);
-   std::string res = des.DES_decryptString(a);
-   std::cout << res;
+    std::string res = des.DES_decryptString(a);
+    std::cout << res;*/
+    std::cout << "Original: " << input << std::endl;
+    Run(input);
+
     /*Run(AES_encryptString, AES_decryptString, "Shelyakina Anastasiya");
     Run(Blowfish_encryptString, Blowfish_decryptString, "Saifullin Bulat");*/
     //Run(des.DES_encryptString, DES_decryptString, "Razin Iliya");
